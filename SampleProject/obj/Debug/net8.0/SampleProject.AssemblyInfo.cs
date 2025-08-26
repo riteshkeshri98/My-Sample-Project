291113eeb3ac297f51cf5999ba0cf443032c79a8
Ritesh keshri <ritesh.keshri@nagarro.com>
2025-08-27 01:10:01 +0530

    latest

--- SampleProject/obj/Debug/net8.0/SampleProject.AssemblyInfo.cs
+++ SampleProject/obj/Debug/net8.0/SampleProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e31f6fa985c1213732f8d43d5fc2812287f8c36")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
